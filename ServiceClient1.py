be407e36b24eff6c0fa89446b2a37d93fe218589
MarcosGonzaga <56901485+MarcosGonzaga@users.noreply.github.com>
2024-02-03 20:00:58 -0300

    Update ServiceClient1.py

--- ServiceClient1.py
+++ ServiceClient1.py
@@ -1,4 +1,5 @@
 import requests
+from urllib.parse import urljoin
 from requests.exceptions import RequestException
 
 # Endpoint do serviço de resolução de nomes
@@ -7,14 +8,14 @@ service_registry_endpoint = 'http://52.21.65.56:5678'
 try:
     # Consulta o serviço de nomes para obter o endpoint do serviço de cálculo de novo salário
     lookup_data = {'name': 'salary_service'}
-    response = requests.get(f'{service_registry_endpoint}/lookup', params=lookup_data)
-    response.raise_for_status()  # Lança uma exceção se a resposta indicar um erro
+    response = requests.get(urljoin(service_registry_endpoint, '/lookup'), params=lookup_data)
+    response.raise_for_status()
 
     salary_service_endpoint = response.json().get('endpoint')
 
     if salary_service_endpoint:
         # Faz uma requisição ao serviço de cálculo de novo salário
-        calculate_salary_endpoint = f'http://{salary_service_endpoint}/calculate_salary'
+        calculate_salary_endpoint = urljoin(f'http://{salary_service_endpoint}', '/calculate_salary')
         calculate_data = {'current_salary': 3000, 'percent_increase': 15}
         response = requests.post(calculate_salary_endpoint, json=calculate_data)
         response.raise_for_status()
@@ -24,14 +25,14 @@ try:
 
         # Consulta o serviço de nomes para obter o endpoint do serviço "employee"
         lookup_data = {'name': 'employee_service'}
-        response = requests.post(f'http://{service_registry_endpoint}/lookup', json=lookup_data)
+        response = requests.post(urljoin(service_registry_endpoint, '/lookup'), json=lookup_data)
         response.raise_for_status()
 
         employee_service_endpoint = response.json().get('endpoint')
 
         if employee_service_endpoint:
             # Atualiza o valor do salário do colaborador de id 201 no serviço "employee"
-            update_salary_endpoint = f'http://{employee_service_endpoint}/empdb/employee/201'
+            update_salary_endpoint = urljoin(f'http://{employee_service_endpoint}', '/empdb/employee/201')
             update_data = {'salary': new_salary}
             response = requests.put(update_salary_endpoint, json=update_data)
             response.raise_for_status()
